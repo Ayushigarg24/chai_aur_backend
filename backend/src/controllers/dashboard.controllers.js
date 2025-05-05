0a8a30cf56f2da7d99011d45c89e50e5b91285a1
ayushigarg24 <ayushigarg24@Ayushi>
2025-05-06 00:26:08 +0530

    added controllers

--- backend/src/controllers/dashboard.controllers.js
+++ backend/src/controllers/dashboard.controllers.js
@@ -1,14 +1,18 @@
-import mongoose from "mongoose"
- import {Video} from "../models/video.model.js"
- import {Subscription} from "../models/subscription.model.js"
- import {Like} from "../models/like.model.js"
+import mongoose, { isValidObjectId } from "mongoose"
+ import {Video} from "../models/video.models.js"
+ import {Subscription} from "../models/subscription.models.js"
+ import {Like} from "../models/like.models.js"
  import {ApiError} from "../utils/ApiError.js"
  import {ApiResponse} from "../utils/ApiResponse.js"
  import {asyncHandler} from "../utils/asyncHandler.js"
  
  const getChannelStats = asyncHandler(async (req, res) => {
      // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
- })
+     const {channelId} = req.params
+     if(!channelId || isValidObjectId(channelId)){
+        throw new ApiError(404,"invalid channelId")
+     }
+})
  
  const getChannelVideos = asyncHandler(async (req, res) => {
      // TODO: Get all the videos uploaded by the channel
